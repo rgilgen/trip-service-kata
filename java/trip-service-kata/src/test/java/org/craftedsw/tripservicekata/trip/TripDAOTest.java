05a167e233098312e62431e7ec6497581b97f1ce
rgilgen <rgilgen@live.com>
2023-11-03 11:27:13 +0100

    Add Static Mock

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripDAOTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripDAOTest.java
@@ -1,5 +1,27 @@
 package org.craftedsw.tripservicekata.trip;
 
-public class TripDAOTest {
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TripDAOTest {
+
+    @Test
+    void testFindTripsByUser() {
+        User user = new User();
+        List<Trip> expectedList;
+
+        try (MockedStatic<TripDAO> mockedStatic = Mockito.mockStatic(TripDAO.class)) {
+            mockedStatic.when(() -> TripDAO.findTripsByUser(user)).thenReturn(Collections.emptyList());
+            expectedList = TripDAO.findTripsByUser(user);
+        }
+
+        assertEquals(0, expectedList.size());
+    }
 }
