f9d1590cdab78a686519d99c3ca4f4106059232d
rgilgen <rgilgen@live.com>
2023-11-03 10:50:17 +0100

    100 % User Test

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -1,5 +1,55 @@
 package org.craftedsw.tripservicekata.user;
 
-public class UserTest {
+import org.craftedsw.tripservicekata.trip.Trip;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UserTest {
+
+ @Test
+ void testGetFriends(){
+  User testee = new User();
+  int expectedSize = 0;
+
+  int actualSize = testee.getFriends().size();
+
+  assertEquals(expectedSize,actualSize);
+ }
+
+ @Test
+ void testAddFriends(){
+  User testee = new User();
+  int expectedSize = 1;
+  int actualSize;
+
+  testee.addFriend(new User());
+  actualSize = testee.getFriends().size();
+
+  assertEquals(expectedSize,actualSize);
+ }
+
+ @Test
+ void testAddTrips(){
+  User testee = new User();
+  int expectedSize = 1;
+  int actualSize;
+
+  testee.addTrip(new Trip());
+  actualSize = testee.trips().size();
+
+  assertEquals(expectedSize,actualSize);
+ }
+
+ @Test
+ void testTrips(){
+  User testee = new User();
+  int expectedSize = 0;
+
+  int actualSize = testee.trips().size();
+
+  assertEquals(expectedSize,actualSize);
+ }
+
 
 }
